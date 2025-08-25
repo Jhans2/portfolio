de1e3c28837ec8b8e3de6629209802181213b2dc
JATIN HANS <you@example.com>
2025-08-25 07:35:26 -0600

    Fix resume PDF path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  
+  // Important for GitHub Pages if repo is not at root
+  base: '/portfolio/',  // <-- your repo name here
+});
